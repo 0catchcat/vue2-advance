358501aa7b391f7689d5e01f8ebaba515a938f30
0catchcat <3438217091@qq.com>
2024-05-14 22:51:23 +0800

    3

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
             // entry: "01_src_分析脚手架/main.js",
             // entry: "02_src_分析ref属性/main.js",
             // entry: "03_src_分析props/main.js",
-            entry: "04_src_mixin混入/main.js",
+            // entry: "04_src_mixin混入/main.js",
+            entry: "07_src_Todolist案例/main.js",
         },
     },
 }
