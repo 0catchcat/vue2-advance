4a3da06b94e8444a577dfa02c6bd0cf436affaf8
0catchcat <3438217091@qq.com>
2024-05-25 22:26:59 +0800

    9

--- 12_src_全局事件总线(GlobalEvent)/main.js
+++ 12_src_全局事件总线(GlobalEvent)/main.js
@@ -1,11 +1,10 @@
 //引入Vue
-import Vue from "vue";
+import Vue from "vue"
 //引入App
-import App from './App';
+import App from "./App"
 
 //关闭Vue的生产提示
-Vue.config.productionTip = false;
-
+Vue.config.productionTip = false
 
 //创建一个vc
 // const Demo = Vue.extend({}); //demo 傀儡不需要配置对象
@@ -15,12 +14,10 @@ Vue.config.productionTip = false;
 // Vue.prototype.x = d;
 
 new Vue({
-    el:'#app',
+    el: "#app",
     render: h => h(App),
     beforeCreate() {
         //此时这个this就是vm，只不过这个时候还并没有去解析模版
-        Vue.prototype.$bus = this; //安装全局事件总线
-    }
-});
-
-
+        Vue.prototype.$bus = this //安装全局事件总线
+    },
+})
